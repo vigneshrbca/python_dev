c9d9595e98c96d42afe5a9c4f63951f4fc32df98
vigneshrbca <vigneshrbca@gmail.com>
2018-04-26 15:36:37 +0000

    version3

--- centrify.py
+++ centrify.py
@@ -1,19 +1,6 @@
 import subprocess
-
-with open("/var/logs/HCLogs/lclog.txt","w") as f:
-     try:
-        cent=subprocess.check_output("rpm -qa | grep -i Centrify",shell=True)
-        if cent != 1:
-           f.write("Centrify is installed")
-           f.write("\n")
-           try:
-              cent_status=subprocess.check_output("adinfo",shell=True)
-              if cent != 1:
-                 f.write("centrify is running")
-              else:
-                 f.write("centrify is not running")
-           except:
-             f.write("Centrify is not ruuning")
-     except:
-        f.write("centrify is not installed")
-       
+cent=subprocess.check_output("rpm -qa | grep -i Centrify",shell=True)
+print(cent)
+print("centrify is installed")
+cent_status= subprocess.call("adinfo",shell=True)   
+          
